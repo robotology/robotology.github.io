d726fe1309a0fb0ad6e8180419d23d51ec578bd4
Daniele E. Domenichelli <daniele.domenichelli@iit.it>
2016-12-02 13:01:01 +0100

    kudos.sh: Several improvements

--- scripts/kudos.sh
+++ scripts/kudos.sh
@@ -9,59 +9,180 @@
 # - curl
 # - jq
 
-if [ $# -eq 0 ]
+if [ $# -lt 2 ]
 then
-    echo "Usage: kudos username [token]"
+    echo "Usage: $0 <organization> <username> [token]"
     exit 1
 fi
 
-function query()
+function queryx()
 {
     local tokenstr=""
-    if [ "$2" != "none" ]; then
-        tokenstr="-H \"Authorization: token $2\""
+    if [ "$3" != "none" ]; then
+        tokenstr="-H \"Authorization: token $3\""
     fi
 
-    curl -s $tokenstr https://api.github.com/search/issues?q=org%3Arobotology+$1 | jq -r .total_count
+    local ret=$(curl -s $tokenstr -G https://api.github.com/search/issues --data-urlencode "q=org:$1 $2" | jq -r .total_count)
+
+    echo $ret
 }
 
-name=$1
+
+function query()
+{
+    q=$(queryx "$1" "$2" "$3")
+    while [ "${q}" == "null" ]; do
+        sleep 5
+        q=$(queryx "$1" "$2" "$3")
+    done
+
+    echo -n $q
+}
+
+
+org=$1
+name=$2
 token="none"
+kudos=0
+
 
 echo ""
-if [ $# -ge 2 ]; then
-    if [[ $2 =~ ^[0-9a-fA-F]+$ ]]; then
-        token=$2
-        echo "Computing kudos for \"$name\" using token \"$token\"..."
+if [ $# -ge 3 ]; then
+    if [[ $3 =~ ^[0-9a-fA-F]+$ ]]; then
+        token=$3
+        echo "Computing kudos for \"$name\" on organization \"$org\" using token \"$token\"..."
     fi
 fi
 
 if [ "$token" == "none" ]; then
-    echo "Computing kudos for \"$name\"..."
+    echo "Computing kudos for \"$name\" on organization \"$org\"..."
 fi
 echo ""
 
 
-q_0=$(query "is%3Aclosed+assignee%3A$name" $token)
-sleep 0.5
-q_1=$(query "is%3Aclosed+-assignee%3A$name+author%3A$name" $token)
-sleep 0.5
-q_2=$(query "is%3Aclosed+-assignee%3A$name+-author%3A$name+involves%3A$name" $token)
-sleep 0.5
-q_3=$(query "is%3Aopen+assignee%3A$name" $token)
-sleep 0.5
-q_4=$(query "is%3Aopen+-assignee%3A$name+author%3A$name" $token)
-sleep 0.5
-q_5=$(query "is%3Aopen+-assignee%3A$name+-author%3A$name+involves%3A$name" $token)
-sleep 0.5
 
-for q_i in q_0 q_1 q_2 q_3 q_4 q_5; do
-    if [ "${!q_i}" == "null" ]; then
-        echo "Wrong username/token or API rate limit exceeded (see https://developer.github.com/v3/#rate-limiting)"
-        exit 2
-    fi
-done
 
-op="kudos = 16 * $q_0 + 8 * $q_1 + 4 * $q_2 + 4 * $q_3 + 2 * $q_4 + $q_5"
-let "$op"
-echo "$op = $kudos"
+
+echo "Closed PRs:"
+
+mult=16
+printf "    author and merged (%02d kudos)                               " $mult
+q=$(query "$org" "is:pr is:merged author:$name" $token)
+let "k = $mult * $q"
+printf "%5d = %5d\n" $q $k
+let "kudos = $kudos + $k"
+
+mult=4
+printf "    author and not merged (rejected, withrawn, etc) (%02d kudos) " $mult
+# "-is:merged" does not work therefore we use closed - merged
+qx=$q
+q=$(query "$org" "is:pr is:closed author:$name" $token)
+let "q = $q - $qx"
+let "k = $mult * $q"
+printf "%5d = %5d\n" $q $k
+let "kudos = $kudos + $k"
+
+mult=8
+printf "    not author and assigned (%02d kudos)                         " $mult
+q=$(query "$org" "is:pr is:closed -author:$name assignee:$name" $token)
+let "k = $mult * $q"
+printf "%5d = %5d\n" $q $k
+let "kudos = $kudos + $k"
+
+mult=4
+printf "    involved (%02d kudos)                                        " $mult
+q=$(query "$org" "is:pr is:closed -author:$name -assignee:$name involves:$name" $token)
+let "k = $mult * $q"
+printf "%5d = %5d\n" $q $k
+let "kudos = $kudos + $k"
+
+
+
+
+echo "Closed issues:"
+
+mult=16
+printf "    assigned (%02d kudos)                                        " $mult
+q=$(query "$org" "is:issue is:closed assignee:$name" $token)
+let "k = $mult * $q"
+printf "%5d = %5d\n" $q $k
+let "kudos = $kudos + $k"
+
+mult=8
+printf "    author and not assigned (%02d kudos)                         " $mult
+q=$(query "$org" "is:issue is:closed -assignee:$name author:$name" $token)
+let "k = $mult * $q"
+printf "%5d = %5d\n" $q $k
+let "kudos = $kudos + $k"
+
+mult=4
+printf "    involves and not author and not assigned (%02d kudos)        " $mult
+q=$(query "$org" "is:issue is:closed -assignee:$name -author:$name involves:$name" $token)
+let "k = $mult * $q"
+printf "%5d = %5d\n" $q $k
+let "kudos = $kudos + $k"
+
+
+
+echo "Open PRs:"
+
+mult=4
+printf "    author (%02d kudos)                                          " $mult
+q=$(query "$org" "is:pr is:open author:$name" $token)
+let "k = $mult * $q"
+printf "%5d = %5d\n" $q $k
+let "kudos = $kudos + $k"
+
+
+mult=2
+printf "    not author and assigned (%02d kudos)                         " $mult
+q=$(query "$org" "is:pr is:open -author:$name assignee:$name" $token)
+let "k = $mult * $q"
+printf "%5d = %5d\n" $q $k
+let "kudos = $kudos + $k"
+
+
+mult=1
+printf "    involved (%02d kudos)                                        " $mult
+q=$(query "$org" "is:pr is:open -author:$name -assignee:$name involves:$name" $token)
+let "k = $mult * $q"
+printf "%5d = %5d\n" $q $k
+let "kudos = $kudos + $k"
+
+
+
+
+
+echo "Open issues:"
+
+mult=2
+printf "    assigned (%02d kudos):                                       " $mult
+q=$(query "$org" "is:issue is:open assignee:$name" $token)
+let "k = $mult * $q"
+printf "%5d = %5d\n" $q $k
+let "kudos = $kudos + $k"
+
+
+mult=1
+printf "    author and not assigned (%02d kudos):                        " $mult
+q=$(query "$org" "is:issue is:open -assignee:$name author:$name" $token)
+let "k = $mult * $q"
+printf "%5d = %5d\n" $q $k
+let "kudos = $kudos + $k"
+
+
+mult=1
+printf "    involves and not author and not assigned (%02d kudos)        " $mult
+q=$(query "$org" "is:issue is:open -assignee:$name -author:$name involves:$name" $token)
+let "k = $mult * $q"
+printf "%5d = %5d\n" $q $k
+let "kudos = $kudos + $k"
+
+
+
+
+
+
+echo "----------------------------------------------------------------------------"
+
+printf "Total kudos                                                            %5d\n" $kudos
